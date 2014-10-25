c55129526a5996ff64ac984792c7850d7493fc8a
Brandon Carroll <brandon@infinitry.net>
2014-10-25 13:52:21 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |----------------------------------------------------------------
+ | Have a Drink!
+ |----------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic
+ | Gulp tasks for your Laravel application. Elixir supports
+ | several common CSS, JavaScript and even testing tools!
+ |
+ */
+
+elixir(function(mix) {
+    mix.sass("app.scss")
+       .scripts([
+            '../resources/assets/js/jquery/jquery.js',
+            '../resources/assets/js/foundation/foundation.js',
+            '../resources/assets/js/app.js'
+         ])
+       .styles('css/app.css')
+       .version(['css/all.css',
+                 'js/all.js'
+               ]);
+});
