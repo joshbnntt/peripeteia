d18453405187ca2e2922db4e2d83e7f51baa85b7
Glenn Hatt <glennhatt@gmail.com>
2014-11-03 22:21:20 +0000

    Added a few fields to the users table

---
+++ app/database/migrations/2014_11_03_215846_add_office_hours_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddOfficeHoursToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			// Comment to test git
+            $table->string('office_hours');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			
+		});
+	}
+
+}
