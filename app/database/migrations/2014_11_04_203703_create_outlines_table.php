39529318d1c16aa32af68103d592e37521d442c0
Glenn Hatt <glennhatt@gmail.com>
2014-11-04 21:00:47 +0000

    created the core tables

---
+++ app/database/migrations/2014_11_04_203703_create_outlines_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOutlinesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('outlines', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('course_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->integer('instructor_id')->unsigned();
+            $table->foreign('instructor_id')->references('id')->on('users');
+            $table->integer('credit_hours');
+            $table->longText('catalog_description')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('course_requirements')->nullable();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('outlines');
+	}
+
+}
